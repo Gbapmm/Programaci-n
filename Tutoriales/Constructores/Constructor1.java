a6b228a6e91690435888e7bf66ef95b228fb7cd4
Gabriel <153546218+Gbapmm@users.noreply.github.com>
2024-01-23 23:02:41 +0100

    Update Constructor1.java

--- Tutoriales/Constructores/Constructor1.java
+++ Tutoriales/Constructores/Constructor1.java
@@ -5,13 +5,13 @@ public class Constructor1 {
     String nombre;
     int edad;
 
-    //Definimos los datos que el Constructor deve devolver
+    //Definimos los datos que el Constructor debe devolver
     public Constructor1(String nombre, int edad){
         nombre = "";
         edad= 0;
     }
 
-    //Hacemos que  se printee el constructor
+    //Hacemos que  se printeen los datos del constructor
     public static void main(String[] args) {
         Constructor1 Persona = new Constructor1("Victor",25);
         System.out.println( Persona.nombre + " " + Persona.edad);
