b2623687d00039127fbbb261868e2a618d69fba1
GABRIEL PAZ MAROÑO <GABRIELPM@iesrodolfoucha.es>
2024-03-11 19:56:51 +0100

    Update Main.java

--- Cadenas/Ejercicio4/Main.java
+++ Cadenas/Ejercicio4/Main.java
@@ -1,46 +1,66 @@
 public class Ejercicio4 {
-    public static void main(String[] args) {
-        String texto = "Este é un exemplo de texto. Inclúe varias frases e parágrafos.\n"
-                + "Outra frase aquí. E un novo parágrafo.\n"
-                + "Última frase do texto.";
+	public static void main(String[] args) {
+		String texto = "Este é un exemplo de texto. Inclúe varias frases e parágrafos.\n"
+				+ "Outra frase aquí. E un novo parágrafo.\n" + "Última frase do texto.";
+		String palabrabuscada = "frase";
 
-        contarFrase(texto);
-    }
+		int apariciones = aparicionesPalabra(texto, palabrabuscada);
 
-    public static void contarFrase(String cadena) {
-        int párrafos = 0;
-        int frases = 0;
-        int palabras = 0;
-        int caracteres = 0;
+		System.out.println("La palabra '" + palabrabuscada + "' aparece " + apariciones + " veces en el texto.");
 
-        for (int i = 0; i < cadena.length(); i++) {
-            char caracter = cadena.charAt(i);
+		contarFrase(texto);
+	}
 
-            if (!Character.isWhitespace(caracter)) {
-                caracteres++;
-            }
+	public static void contarFrase(String texto) {
+		int párrafos = 0;
+		int frases = 0;
+		int palabras = 0;
+		int caracteres = 0;
 
-            if (caracter == '\n') {
-                párrafos++;
-            }
+		for (int i = 0; i < texto.length(); i++) {
+			char caracter = texto.charAt(i);
 
-            if (Character.isWhitespace(caracter)) {
-                if (i > 0 && cadena.charAt(i - 1) == '.') {
-                    frases++;
-                }
-            }
-        }
+			if (!Character.isWhitespace(caracter)) {
+				caracteres++;
+			}
 
-        if (cadena.endsWith(".")) {
-            frases++;
-        }
+			if (caracter == '\n') {
+				párrafos++;
+			}
 
-        String[] palabrasDiv = cadena.split("\\s+");
-        palabras = palabrasDiv.length;
+			if (Character.isWhitespace(caracter)) {
+				if (i > 0 && texto.charAt(i - 1) == '.') {
+					frases++;
+				}
+			}
+		}
 
-        System.out.println("Número de párrafos: " + párrafos);
-        System.out.println("Número de frases: " + frases);
-        System.out.println("Número de palabras: " + palabras);
-        System.out.println("Número de caracteres: " + caracteres);
-    }
+		if (texto.endsWith(".")) {
+			frases++;
+		}
+
+		String[] palabrasDiv = texto.split("\\s+");
+		palabras = palabrasDiv.length;
+
+		System.out.println("Número de párrafos: " + párrafos);
+		System.out.println("Número de frases: " + frases);
+		System.out.println("Número de palabras: " + palabras);
+		System.out.println("Número de caracteres: " + caracteres);
+	}
+
+	public static int aparicionesPalabra(String texto, String palabrabuscada) {
+		String textoLower = texto.toLowerCase();
+		String palabraLower = palabrabuscada.toLowerCase();
+
+		int apariciones = 0;
+		int indice = 0;
+
+		while ((indice = textoLower.indexOf(palabraLower, indice)) != -1) {
+			// a
+			apariciones++;
+			indice++;
+		}
+
+		return apariciones;
+	}
 }
