66a3c0ccb3a3781c2951460a2d87065d96e09b04
Gabriel <153546218+Gbapmm@users.noreply.github.com>
2024-03-14 13:04:53 +0100

    Add files via upload

---
+++ Ejercicios con Java/Tutoriales/ExcepcionEnteros/ExcepcionEnteros.java
@@ -0,0 +1,11 @@
+package EdadUsuario;
+
+public class ExcepcionEnteros extends Exception {
+    public ExcepcionEnteros() {
+        super();
+    }
+
+    public ExcepcionEnteros(String mensaje) {
+        super(mensaje);
+    }
+}
---
+++ Ejercicios con Java/Tutoriales/ExcepcionEnteros/Main.java
@@ -0,0 +1,31 @@
+package EdadUsuario;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.println("Introduzca su edad: ");
+
+                if (!scanner.hasNextInt()) {
+                    throw new ExcepcionEnteros("Error: La entrada no es un número entero.");
+                }
+
+                int edad = scanner.nextInt();
+
+                if (edad < 0) {
+                    throw new ExcepcionEnteros("Error: La edad no puede ser negativa.");
+                }
+
+                System.out.println("Su edad es de: " + edad);
+                break; // Si se logra convertir correctamente, se sale del bucle
+            } catch (ExcepcionEnteros e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        scanner.close();
+    }
+}
